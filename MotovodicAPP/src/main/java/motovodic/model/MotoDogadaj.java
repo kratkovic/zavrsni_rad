19116015019f27ff3f0da931dc500459b5761612
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2023-02-28 21:38:20 +0100

    motodogadaj

---
+++ MotovodicAPP/src/main/java/motovodic/model/MotoDogadaj.java
@@ -0,0 +1,79 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package motovodic.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.util.Date;
+
+
+@Entity
+public class MotoDogadaj extends Entitet{
+    
+
+private String naziv;
+private String mjestoodrzavanja;
+private String odgovorniclan;
+private Date datumpocetka;
+@ManyToOne
+@JoinColumn(name = "motoklub_sifra")
+private MotoKlub motoklub;
+
+public MotoDogadaj(){
+    super();
+}
+
+    public MotoDogadaj(String naziv, String mjestoodrzavanja, String odgovorniclan, Date datumpocetka, MotoKlub motoklub, int sifra) {
+        super(sifra);
+        this.naziv = naziv;
+        this.mjestoodrzavanja = mjestoodrzavanja;
+        this.odgovorniclan = odgovorniclan;
+        this.datumpocetka = datumpocetka;
+        this.motoklub = motoklub;
+    }
+
+    
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public String getMjestoodrzavanja() {
+        return mjestoodrzavanja;
+    }
+
+    public void setMjestoodrzavanja(String mjestoodrzavanja) {
+        this.mjestoodrzavanja = mjestoodrzavanja;
+    }
+
+    public String getOdgovorniclan() {
+        return odgovorniclan;
+    }
+
+    public void setOdgovorniclan(String odgovorniclan) {
+        this.odgovorniclan = odgovorniclan;
+    }
+
+    public Date getDatumpocetka() {
+        return datumpocetka;
+    }
+
+    public void setDatumpocetka(Date datumpocetka) {
+        this.datumpocetka = datumpocetka;
+    }
+
+    public MotoKlub getMotoklub() {
+        return motoklub;
+    }
+
+    public void setMotoklub(MotoKlub motoklub) {
+        this.motoklub = motoklub;
+    }
+
+}
--- MotovodicAPP/src/main/java/motovodic/model/MotoKlub.java
+++ MotovodicAPP/src/main/java/motovodic/model/MotoKlub.java
@@ -4,7 +4,69 @@
  */
 package motovodic.model;
 
+import jakarta.persistence.Entity;
 
-public class MotoKlub {
+
+@Entity
+public class MotoKlub extends Entitet{
+
+  
+    private String naziv;
+    private String mjesto;
+    private Integer brojclanova;
+    private boolean registracija;
     
+    
+    
+    public MotoKlub(){
+        super();
+    }
+
+    public MotoKlub(String naziv, String mjesto, Integer brojclanova, boolean registracija) {
+        this.naziv = naziv;
+        this.mjesto = mjesto;
+        this.brojclanova = brojclanova;
+        this.registracija = registracija;
+    }
+            
+    
+    
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public String getMjesto() {
+        return mjesto;
+    }
+
+    public void setMjesto(String mjesto) {
+        this.mjesto = mjesto;
+    }
+
+    public Integer getBrojclanova() {
+        return brojclanova;
+    }
+
+    public void setBrojclanova(Integer brojclanova) {
+        this.brojclanova = brojclanova;
+    }
+
+    public boolean isRegistracija() {
+        return registracija;
+    }
+
+    public void setRegistracija(boolean registracija) {
+        this.registracija = registracija;
+    }
+    
+    @Override
+	public String toString() {
+		
+		return naziv + " " + mjesto;
+
+}
 }
