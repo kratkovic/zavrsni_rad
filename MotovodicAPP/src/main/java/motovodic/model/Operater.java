0a695f1815d99b3e479ce45fd49c3beb3fa2a2af
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2023-03-16 21:52:03 +0100

    operater napravljen

--- MotovodicAPP/src/main/java/motovodic/model/Operater.java
+++ MotovodicAPP/src/main/java/motovodic/model/Operater.java
@@ -4,10 +4,52 @@
  */
 package motovodic.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 
+@Entity
 public class Operater extends Entitet{
     
+    private String ime;
+    private String prezime;
+    private String email;
+    @Column(columnDefinition = "char(61)")
     private char[] lozinka;
     
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPrezime() {
+        return prezime;
+    }
+
+    public void setPrezime(String prezime) {
+        this.prezime = prezime;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public char[] getLozinka() {
+        return lozinka;
+    }
+
+    public void setLozinka(char[] lozinka) {
+        this.lozinka = lozinka;
+    }
+
+    public String getImePrezime(){
+        return getIme() + " " + getPrezime();
+    }
     
 }
