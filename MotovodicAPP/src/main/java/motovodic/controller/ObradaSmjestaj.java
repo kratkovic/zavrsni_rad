e49f22557d4a5435aaa1d4bb003c3e9e3994ba80
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2023-03-21 21:50:10 +0100

    obrada smjestaj

---
+++ MotovodicAPP/src/main/java/motovodic/controller/ObradaSmjestaj.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package motovodic.controller;
+
+import java.util.List;
+import motovodic.model.Smjestaj;
+import motovodic.util.MotoVodicException;
+
+
+public class ObradaSmjestaj extends Obrada<Smjestaj>{
+
+    @Override
+    public List<Smjestaj> read() {
+        return session.createQuery("from Smjestaj", Smjestaj.class).list();
+    }
+
+    @Override
+    protected void kontrolaUnos() throws MotoVodicException {
+        kontrolaNaziv();
+    }
+
+    @Override
+    protected void kontrolaPromjena() throws MotoVodicException {
+        kontrolaNazivNull();
+        kontrolaNazivNijeBroj();
+        kontrolaNazivMinimalnaDuzina();
+        kontrolaNazivMaksimalnaDuzina();
+        kontrolaNazivDupliUBazi();
+    }
+
+    @Override
+    protected void kontrolaBrisanje() throws MotoVodicException {
+        
+    }
+
+    private void kontrolaNaziv() throws MotoVodicException{
+        kontrolaNazivNull();
+        kontrolaNazivNijeBroj();
+        kontrolaNazivMinimalnaDuzina();
+        kontrolaNazivMaksimalnaDuzina();
+        kontrolaNazivDupliUBazi();
+    }
+
+    private void kontrolaNazivNull() throws MotoVodicException{
+       if (entitet.getNaziv() == null) {
+            throw new MotoVodicException("Naziv mora biti postavljen");
+    }
+
+    }
+
+    private void kontrolaNazivNijeBroj()throws MotoVodicException{
+        boolean broj = false;
+        try {
+            Double.parseDouble(entitet.getNaziv());
+            broj = true;
+        } catch (Exception e) {
+        }
+
+        if (broj) {
+            throw new MotoVodicException("Naziv smještaja ne smije biti broj");
+    }
+}
+
+    private void kontrolaNazivMinimalnaDuzina() throws MotoVodicException{
+        if (entitet.getNaziv().trim().length() < 3) {
+            throw new MotoVodicException("Naziv smještaja mora imati minimalno 3 znaka");
+        }
+    }
+
+    private void kontrolaNazivMaksimalnaDuzina() throws MotoVodicException{
+       if (entitet.getNaziv().trim().length() > 50) {
+            throw new MotoVodicException("Naziv smještaja može imati minimalno 50 znakova");
+        }
+    }
+
+    private void kontrolaNazivDupliUBazi() throws MotoVodicException{
+        List<Smjestaj> smjestaji = null;
+         try {
+            smjestaji = session.createQuery("from Smjestaj sm "
+                    + " where sm.naziv=:naziv",
+                    Smjestaj.class)
+                    .setParameter("naziv", entitet.getNaziv())
+                    .list();
+        } catch (Exception e) {
+        }
+        if (smjestaji != null && !smjestaji.isEmpty()) {
+            throw new MotoVodicException("Smještaj s istim nazivom postoji u bazi");
+        }
+    }
+      
+    }
