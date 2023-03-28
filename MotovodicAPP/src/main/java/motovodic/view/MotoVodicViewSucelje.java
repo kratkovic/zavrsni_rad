ccd3b9c854a894692155dc1bd39fc44f74f53663
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2023-03-28 21:02:39 +0200

    kreiranje view sucelja Moto dogadjaj

---
+++ MotovodicAPP/src/main/java/motovodic/view/MotoVodicViewSucelje.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package motovodic.view;
+
+/**
+ *
+ * @author Kiki
+ */
+public interface MotoVodicViewSucelje {
+    
+    public void ucitaj();
+    public void napuniView();
+    public void napuniModel();
+}
--- MotovodicAPP/src/main/java/motovodic/view/ProzorMotoDogadjaj.java
+++ MotovodicAPP/src/main/java/motovodic/view/ProzorMotoDogadjaj.java
@@ -8,7 +8,7 @@ package motovodic.view;
  *
  * @author Kiki
  */
-public class ProzorMotoDogadjaj extends javax.swing.JFrame {
+public class ProzorMotoDogadjaj extends javax.swing.JFrame implements MotoVodicViewSucelje{
 
     /**
      * Creates new form ProzorMotoDogadjaj
@@ -26,7 +26,7 @@ public class ProzorMotoDogadjaj extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -40,43 +40,27 @@ public class ProzorMotoDogadjaj extends javax.swing.JFrame {
         );
 
         pack();
-        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
+
+    @Override
+    public void ucitaj() {
+       
+    }
+
+    @Override
+    public void napuniView() {
+       
+    }
+
+    @Override
+    public void napuniModel() {
+        
+    }
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(ProzorMotoDogadjaj.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(ProzorMotoDogadjaj.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(ProzorMotoDogadjaj.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(ProzorMotoDogadjaj.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new ProzorMotoDogadjaj().setVisible(true);
-            }
-        });
-    }
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     // End of variables declaration//GEN-END:variables
