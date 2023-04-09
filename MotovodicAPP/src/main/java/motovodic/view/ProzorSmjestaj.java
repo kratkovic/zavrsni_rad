5f7ef2a2b3958ef9c3977dedb0054997ae70bf4d
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2023-04-09 21:15:45 +0200

    doradivanje prozora smještaj

--- MotovodicAPP/src/main/java/motovodic/view/ProzorSmjestaj.java
+++ MotovodicAPP/src/main/java/motovodic/view/ProzorSmjestaj.java
@@ -416,6 +416,7 @@ public class ProzorSmjestaj extends javax.swing.JFrame {
             } else {
                 s.setServis(null);
             }
+            
         }
     }
     /**
@@ -439,8 +440,6 @@ public class ProzorSmjestaj extends javax.swing.JFrame {
     private javax.swing.JTextField txtVrsta;
     // End of variables declaration//GEN-END:variables
 
-    private void cmbSmjestajiItemStateChanged() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
-    }
+   
 
 }
