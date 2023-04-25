bec04e99685982d6b3b3a3e99d3c6f1d4872176a
Kristijan Ratkovic <ratkovic.kristijan@gmail.com>
2023-04-25 16:53:00 +0200

    pocetni insert

--- MotovodicAPP/src/main/java/motovodic/util/PocetniInsert.java
+++ MotovodicAPP/src/main/java/motovodic/util/PocetniInsert.java
@@ -15,24 +15,21 @@ import motovodic.model.Servis;
 import motovodic.model.Smjestaj;
 import org.hibernate.Session;
 
-
 public class PocetniInsert {
-    
+
     private static final int BROJ_MOTOKLUBOVA = 30;
     private static final int BROJ_MOTODOGADJAJA = 80;
     private static final int BROJ_SMJESTAJA = 100;
     private static final int BROJ_SERVISA = 20;
-    
+
     private Faker faker;
     private List<MotoKlub> motoklubovi;
     private List<MotoDogadjaj> motodogadaji;
     private List<Servis> servisi;
     private List<Smjestaj> smjestaji;
     private Session session;
-    
-    
-    
-    public PocetniInsert(){
+
+    public PocetniInsert() {
         faker = new Faker();
         motoklubovi = new ArrayList<>();
         motodogadaji = new ArrayList<>();
@@ -44,14 +41,22 @@ public class PocetniInsert {
         kreirajMotoDogadjaje();
         kreirajServise();
         kreirajSmjestaje();
-        
+
         session.getTransaction().commit();
         HibernateUtil.CloseSession();
     }
-    
-    private void kreirajMotoKlubove(){
+
+    public static int brojIzmedju(int min, int max) {
+        return ThreadLocalRandom.current().nextInt(min, max + 1);
+    }
+
+    public static String radnoVrijeme() {
+        return brojIzmedju(5, 9) + "-" + brojIzmedju(14, 17);
+    }
+
+    private void kreirajMotoKlubove() {
         MotoKlub mk;
-        for(int i = 0; i<BROJ_MOTOKLUBOVA; i++){
+        for (int i = 0; i < BROJ_MOTOKLUBOVA; i++) {
             mk = new MotoKlub();
             mk.setNaziv(faker.app().name());
             mk.setMjesto(faker.app().name());
@@ -63,47 +68,47 @@ public class PocetniInsert {
     }
 
     private void kreirajMotoDogadjaje() {
-       MotoDogadjaj md;
-       for(int i = 0; i <BROJ_MOTODOGADJAJA; i++){
-           md = new MotoDogadjaj();
-           md.setMotoklub(motoklubovi.get(sb(0,motoklubovi.size()-1)));
-           md.setNaziv(faker.app().name());
-           md.setDatumpocetka(faker.date().birthday(1, 12));
-           md.setMjestoodrzavanja(faker.app().name());
-           md.setOdgovorniclan(faker.name().firstName() + " " + faker.name().lastName());
-           session.persist(md);
-           motodogadaji.add(md);
-           
-       }
+        MotoDogadjaj md;
+        for (int i = 0; i < BROJ_MOTODOGADJAJA; i++) {
+            md = new MotoDogadjaj();
+            md.setMotoklub(motoklubovi.get(sb(0, motoklubovi.size() - 1)));
+            md.setNaziv(faker.app().name());
+            md.setDatumpocetka(faker.date().birthday(1, 12));
+            md.setMjestoodrzavanja(faker.app().name());
+            md.setOdgovorniclan(faker.name().firstName() + " " + faker.name().lastName());
+            session.persist(md);
+            motodogadaji.add(md);
+
+        }
     }
 
     private void kreirajSmjestaje() {
-       Smjestaj s;
-       for(int i=0; i<BROJ_SMJESTAJA; i++){
-           s = new Smjestaj();
-           s.setNaziv(faker.app().name());
-           s.setVrsta(faker.app().name());
-           s.setMotoDogadjaj(motodogadaji.get(sb(0,motodogadaji.size()-1)));
-           s.setServis(servisi.get(sb(0,servisi.size()-1)));
-           s.setCijena(new BigDecimal(faker.number().numberBetween(5, 200)));
-           session.persist(s);
-           smjestaji.add(s);
-       }
+        Smjestaj s;
+        for (int i = 0; i < BROJ_SMJESTAJA; i++) {
+            s = new Smjestaj();
+            s.setNaziv(faker.app().name());
+            s.setVrsta(faker.app().name());
+            s.setMotoDogadjaj(motodogadaji.get(sb(0, motodogadaji.size() - 1)));
+            s.setServis(servisi.get(sb(0, servisi.size() - 1)));
+            s.setCijena(new BigDecimal(faker.number().numberBetween(5, 200)));
+            session.persist(s);
+            smjestaji.add(s);
+        }
     }
 
     private void kreirajServise() {
-       Servis sr;
-       for(int i = 0; i<BROJ_SERVISA; i++){
-           sr = new Servis();
-         sr.setNaziv(faker.app().name());
-         sr.setMjesto(faker.address().fullAddress());
-         sr.setRadnovrijeme(faker.app().name());
-         session.persist(sr);
-         servisi.add(sr);
-       }
+        Servis sr;
+        for (int i = 0; i < BROJ_SERVISA; i++) {
+            sr = new Servis();
+            sr.setNaziv(faker.app().name());
+            sr.setMjesto(faker.address().fullAddress());
+            sr.setRadnovrijeme(radnoVrijeme());
+            session.persist(sr);
+            servisi.add(sr);
+        }
     }
-    
-    private int sb(int min, int max){
+
+    private int sb(int min, int max) {
         return ThreadLocalRandom.current().nextInt(min, max + 1);
     }
 }
